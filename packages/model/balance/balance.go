d80bb24d7be5f15826cbe1e53e16b33b629123be
capossele <angelo@Angelo.local>
2019-06-19 23:13:58 +0100

    balance unit test added

--- packages/model/balance/balance.go
+++ packages/model/balance/balance.go
@@ -1,10 +1,10 @@
 package balance
 
 import (
+	"encoding/binary"
 	"sync"
 
 	"github.com/iotaledger/goshimmer/packages/errors"
-	"github.com/iotaledger/goshimmer/packages/ternary"
 )
 
 type Entry struct {
@@ -15,14 +15,79 @@ type Entry struct {
 	modified       bool
 }
 
-func New(hash ternary.Trytes) *Entry {
+func New() *Entry {
 	return &Entry{}
 }
 
+// region public methods with locking //////////////////////////////////////////////////////////////////////////////////
+
+func (balanceEntry *Entry) GetValue() (result int64) {
+	balanceEntry.valueMutex.RLock()
+	result = balanceEntry.value
+	balanceEntry.valueMutex.RUnlock()
+
+	return
+}
+
+func (balanceEntry *Entry) SetValue(value int64) {
+	balanceEntry.valueMutex.Lock()
+	balanceEntry.value = value
+	balanceEntry.valueMutex.Unlock()
+
+	balanceEntry.modified = true
+}
+
+func (balanceEntry *Entry) GetTimestamp() (result uint64) {
+	balanceEntry.timestampMutex.RLock()
+	result = balanceEntry.timestamp
+	balanceEntry.timestampMutex.RUnlock()
+
+	return
+}
+
+func (balanceEntry *Entry) SetTimestamp(timestamp uint64) {
+	balanceEntry.timestampMutex.Lock()
+	balanceEntry.timestamp = timestamp
+	balanceEntry.timestampMutex.Unlock()
+
+	balanceEntry.modified = true
+}
+
+func (balanceEntry *Entry) GetModified() bool {
+	return true
+}
+
+func (balanceEntry *Entry) SetModified(modified bool) {
+}
+
 func (balanceEntry *Entry) Marshal() (result []byte) {
+	result = make([]byte, MARSHALED_ENTRY_VALUE_SIZE+MARSHALED_ENTRY_TIMESTAMP_SIZE)
+
+	balanceEntry.valueMutex.RLock()
+	binary.BigEndian.PutUint64(result[MARSHALED_ENTRY_VALUE_START:MARSHALED_ENTRY_VALUE_END], uint64(balanceEntry.value))
+	balanceEntry.valueMutex.RUnlock()
+
+	balanceEntry.timestampMutex.RLock()
+	binary.BigEndian.PutUint64(result[MARSHALED_ENTRY_TIMESTAMP_START:MARSHALED_ENTRY_TIMESTAMP_END], uint64(balanceEntry.timestamp))
+	balanceEntry.timestampMutex.RUnlock()
+
 	return
 }
 
 func (balanceEntry *Entry) Unmarshal(data []byte) (err errors.IdentifiableError) {
+	dataLen := len(data)
+
+	if dataLen < MARSHALED_ENTRY_VALUE_SIZE+MARSHALED_ENTRY_TIMESTAMP_SIZE {
+		return ErrMarshallFailed.Derive(errors.New("unmarshall failed"), "marshaled balanceEntry are too short")
+	}
+
+	balanceEntry.valueMutex.Lock()
+	balanceEntry.value = int64(binary.BigEndian.Uint64(data[MARSHALED_ENTRY_VALUE_START:MARSHALED_ENTRY_VALUE_END]))
+	balanceEntry.valueMutex.Unlock()
+
+	balanceEntry.timestampMutex.Lock()
+	balanceEntry.timestamp = binary.BigEndian.Uint64(data[MARSHALED_ENTRY_TIMESTAMP_START:MARSHALED_ENTRY_TIMESTAMP_END])
+	balanceEntry.timestampMutex.Unlock()
+
 	return
 }
--- packages/model/balance/balance_test.go
+++ packages/model/balance/balance_test.go
@@ -1,1 +1,32 @@
 package balance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestBalance_SettersGetters(t *testing.T) {
+	balance := New()
+	balance.SetValue(int64(^uint64(0) >> 1))
+	balance.SetTimestamp(^uint64(0))
+
+	assert.Equal(t, balance.GetValue(), int64(^uint64(0)>>1), "MaxValue")
+	assert.Equal(t, balance.GetTimestamp(), ^uint64(0), "MaxTimestamp")
+}
+
+func TestBalance_MarshalUnmarshalGetters(t *testing.T) {
+	balance := New()
+	balance.SetValue(int64(^uint64(0) >> 1))
+	balance.SetTimestamp(^uint64(0))
+
+	balanceByte := balance.Marshal()
+	balanceUnmarshaled := New()
+	err := balanceUnmarshaled.Unmarshal(balanceByte)
+	if err != nil {
+		fmt.Println(err, len(balanceByte))
+	}
+	assert.Equal(t, balanceUnmarshaled.GetValue(), balance.GetValue(), "Value")
+	assert.Equal(t, balanceUnmarshaled.GetTimestamp(), balance.GetTimestamp(), "Timestamp")
+}
