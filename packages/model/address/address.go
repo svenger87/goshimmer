162bb83fde3b343c10048138a7c07da66f8f96d3
capossele <angelo@Angelo.local>
2019-06-20 08:07:50 +0100

    address unit test added

--- packages/model/address/address.go
+++ packages/model/address/address.go
@@ -1,21 +1,22 @@
 package address
 
 import (
+	"encoding/binary"
+	"strconv"
 	"sync"
 
 	"github.com/iotaledger/goshimmer/packages/errors"
 	"github.com/iotaledger/goshimmer/packages/model/balance"
 	"github.com/iotaledger/goshimmer/packages/ternary"
+	"github.com/iotaledger/goshimmer/packages/typeutils"
 )
 
 type Entry struct {
-	addressShard      ternary.Trytes
-	addressShardMutex sync.RWMutex
-	accumulated       int64
-	accumulatedMutex  sync.RWMutex
-	history           []balance.Entry
-	historyMutex      sync.RWMutex
-	modified          bool
+	addressShard ternary.Trytes
+	accumulated  int64
+	history      []*balance.Entry
+	historyMutex sync.RWMutex
+	modified     bool
 }
 
 func New(addressShard ternary.Trytes) *Entry {
@@ -25,18 +26,97 @@ func New(addressShard ternary.Trytes) *Entry {
 	}
 }
 
+func (addressEntry *Entry) GetAddressShard() (result ternary.Trytes) {
+	result = addressEntry.addressShard
+	return
+}
+
+func (addressEntry *Entry) GetBalance() (result int64) {
+	addressEntry.historyMutex.RLock()
+	defer addressEntry.historyMutex.RUnlock()
+
+	result = addressEntry.accumulated
+	return
+}
+
+func (addressEntry *Entry) GetModified() bool {
+	return true
+}
+
+func (addressEntry *Entry) SetModified(modified bool) {
+}
+
+func (addressEntry *Entry) Add(balanceEntries ...*balance.Entry) {
+	addressEntry.historyMutex.Lock()
+	defer addressEntry.historyMutex.Unlock()
+
+	addressEntry.history = append(addressEntry.history, balanceEntries...)
+
+	for _, balanceEntry := range balanceEntries {
+		addressEntry.accumulated += balanceEntry.GetValue()
+	}
+	addressEntry.modified = true
+}
+
 func (addressEntry *Entry) Marshal() (result []byte) {
+	result = make([]byte, MARSHALED_ENTRY_MIN_SIZE+len(addressEntry.history)*MARSHALED_BALANCE_ENTRY_SIZE)
+
+	addressEntry.historyMutex.RLock()
+
+	binary.BigEndian.PutUint64(result[MARSHALED_ENTRY_HISTORY_COUNT_START:MARSHALED_ENTRY_HISTORY_COUNT_END], uint64(len(addressEntry.history)))
+
+	binary.BigEndian.PutUint64(result[MARSHALED_ENTRY_ACCUMULATED_START:MARSHALED_ENTRY_ACCUMULATED_END], uint64(addressEntry.accumulated))
+
+	copy(result[MARSHALED_ENTRY_ADDRESS_START:MARSHALED_ENTRY_ADDRESS_END], addressEntry.addressShard.CastToBytes())
+
+	i := 0
+	for _, balanceEntry := range addressEntry.history {
+		var BALANCE_START = MARSHALED_ENTRY_HISTORY_START + i*(MARSHALED_BALANCE_ENTRY_SIZE)
+		var BALANCE_END = BALANCE_START + MARSHALED_BALANCE_ENTRY_SIZE
+
+		copy(result[BALANCE_START:BALANCE_END], balanceEntry.Marshal())
+
+		i++
+	}
+
+	addressEntry.historyMutex.RUnlock()
 	return
 }
 
 func (addressEntry *Entry) Unmarshal(data []byte) (err errors.IdentifiableError) {
+	dataLen := len(data)
+
+	if dataLen <= MARSHALED_ENTRY_MIN_SIZE {
+		return ErrMarshallFailed.Derive(errors.New("unmarshall failed"), "marshaled address are too short")
+	}
+
+	balancesCount := binary.BigEndian.Uint64(data[MARSHALED_ENTRY_HISTORY_COUNT_START:MARSHALED_ENTRY_HISTORY_COUNT_END])
+
+	if dataLen < MARSHALED_ENTRY_MIN_SIZE+int(balancesCount)*MARSHALED_BALANCE_ENTRY_SIZE {
+		return ErrMarshallFailed.Derive(errors.New("unmarshall failed"), "marshaled address are too short for "+strconv.FormatUint(balancesCount, 10)+" balances")
+	}
+
+	addressEntry.historyMutex.Lock()
+
+	addressEntry.addressShard = ternary.Trytes(typeutils.BytesToString(data[MARSHALED_ENTRY_ADDRESS_START:MARSHALED_ENTRY_ADDRESS_END]))
+	addressEntry.accumulated = int64(binary.BigEndian.Uint64(data[MARSHALED_ENTRY_ACCUMULATED_START:MARSHALED_ENTRY_ACCUMULATED_END]))
+	addressEntry.history = make([]*balance.Entry, balancesCount)
+	for i := uint64(0); i < balancesCount; i++ {
+		var BALANCE_START = MARSHALED_ENTRY_HISTORY_START + i*(MARSHALED_BALANCE_ENTRY_SIZE)
+		var BALANCE_END = BALANCE_START + MARSHALED_BALANCE_ENTRY_SIZE
+
+		addressEntry.history[i] = balance.New()
+		err = addressEntry.history[i].Unmarshal(data[BALANCE_START:BALANCE_END])
+	}
+
+	addressEntry.historyMutex.Unlock()
 	return
 }
 
-func getAddressFromDatabase(addressShard ternary.Trytes) (*Entry, errors.IdentifiableError) {
-	return nil, nil
-}
+// func getAddressFromDatabase(addressShard ternary.Trytes) (*Entry, errors.IdentifiableError) {
+// 	return nil, nil
+// }
 
-func databaseContainsAddress(addressShard ternary.Trytes) (bool, errors.IdentifiableError) {
-	return false, nil
-}
+// func databaseContainsAddress(addressShard ternary.Trytes) (bool, errors.IdentifiableError) {
+// 	return false, nil
+// }
--- packages/model/address/address_test.go
+++ packages/model/address/address_test.go
@@ -1,1 +1,39 @@
 package address
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/iotaledger/goshimmer/packages/model/balance"
+	"github.com/iotaledger/goshimmer/packages/ternary"
+	"github.com/magiconair/properties/assert"
+)
+
+func TestAddress_SettersGetters(t *testing.T) {
+	address := ternary.Trytes("A9999999999999999999999999999999999999999999999999999999999999999999999999999999F")
+	shardMarker := ternary.Trytes("NPHTQORL9XKA")
+	addressShard := New(address + shardMarker)
+
+	balanceEntries := []*balance.Entry{balance.NewValue(100, 1), balance.NewValue(100, 2)}
+
+	addressShard.Add(balanceEntries...)
+	assert.Equal(t, addressShard.GetBalance(), int64(200), "Accumulated")
+}
+
+func TestBalance_MarshalUnmarshalGetters(t *testing.T) {
+	address := ternary.Trytes("A9999999999999999999999999999999999999999999999999999999999999999999999999999999F")
+	shardMarker := ternary.Trytes("NPHTQORL9XKA")
+	addressShard := New(address + shardMarker)
+
+	balanceEntries := []*balance.Entry{balance.NewValue(100, 1), balance.NewValue(100, 2)}
+
+	addressShard.Add(balanceEntries...)
+
+	addressShardByte := addressShard.Marshal()
+	addressShardUnmarshaled := &Entry{}
+	err := addressShardUnmarshaled.Unmarshal(addressShardByte)
+	if err != nil {
+		fmt.Println(err, len(addressShardByte))
+	}
+	assert.Equal(t, addressShardUnmarshaled.GetBalance(), addressShard.GetBalance(), "Accumulated")
+}
